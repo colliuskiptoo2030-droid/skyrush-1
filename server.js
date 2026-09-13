9d3ea976044b10ea6582201fdd48e19a5c24ec4e
colliuskiptoo2030-droid <colliuskiptoo2030@gmail.com>
2026-09-13 19:18:22 +0300

    Refactor server.js to include axios and improve structure

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+
 const app = express();
 
 app.use(express.json());
